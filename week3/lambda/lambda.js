9f14c47faf4a75ff1889ade8b633208eeaa11b61
Dierk Koenig <dierk.koenig@canoo.com>
2019-10-01 14:28:13 +0200

    before week 3

--- week3/lambda/lambda.js
+++ week3/lambda/lambda.js
@@ -1,36 +1,4 @@
-// atoms
-const id    = x =>      x;
-const konst = x => y => x;
 
-
-// derived
-const fst  = konst;
-const snd  = konst(id);
-
-const T = fst;
-const F = snd;
-
-const and = p => q => p(q)(p);
-
-const M   = f => f(f);
-const or  = M;
-
-const Pair = x => y => selector => selector(x)(y);
-const firstname = fst;
-const lastname  = snd;
-
-const Triple = x => y => z => f => f(x)(y)(z);
-const tfirstname = x => y => z => x;
-const tlastname  = x => y => z => y;
-const tage       = x => y => z => z;
-
-
-
-// Either
-
-const Left   = x => f => g => f(x);
-const Right  = x => f => g => g(x);
-const either = e => f => g => e (f) (g);
 
 
 // ----- special -----
